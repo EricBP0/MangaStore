09c299df0fc6b39c453a1f93771d48d0f2d3840c
EricBP <ericborgep@gmail.com>
2023-05-30 17:05:26 -0300

    commit comentario programa

--- src/main/java/com/example/mangastoreservidor/HelloApplication.java
+++ src/main/java/com/example/mangastoreservidor/HelloApplication.java
@@ -15,7 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        //Pedro é viado
     }
 
     public static void main(String[] args) {
