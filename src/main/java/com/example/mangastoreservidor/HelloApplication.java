9a248d3c177b5ba6dd38a8b61b26b05ca7dce2c2
EricBP <ericborgep@gmail.com>
2023-05-30 16:06:09 -0300

    Commit teste

--- src/main/java/com/example/mangastoreservidor/HelloApplication.java
+++ src/main/java/com/example/mangastoreservidor/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        //Rodrigo é o bita
     }
 
     public static void main(String[] args) {
